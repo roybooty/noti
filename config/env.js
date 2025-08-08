aa55c2514b048e31c4891cd6d8d443216e0d2bb5
roybooty <safwanidris4@proton.me>
2025-08-08 17:40:34 +0000

    fixed enviromental git problem

--- config/env.js
+++ config/env.js
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 
 
-config({ path: `.env.${process.env.NODENV || 'development'}.local`});
+config({ path: `.env.${process.env.NODENV || 'development'}`});
 
 export const { PORT, NODENV, DB_URI } = process.env
